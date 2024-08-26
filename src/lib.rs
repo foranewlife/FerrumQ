09541ebc4d35d7fa0ce93a311389269a66877c3f
Yongshuo Chen <627447561@qq.com>
2024-08-26 09:14:21 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use pyo3::prelude::*;
+mod start {
+    pub mod hello;
+}
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn ferrum_q(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(start::hello::print_hello, m)?)?;
+    Ok(())
+}
